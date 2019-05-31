d42a1fc1eb8f67ccec20569b874d2fd06100aef8
khellwan <thiagobispinho@hotmail.com>
2019-05-31 18:55:31 -0300

    Added Scene Changer

---
+++ levels/Door.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+export(String, FILE, "*.tscn") var world_scene
+
+var changed = false
+
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	for body in bodies:
+		if (body.name == "PlayerBody" and not changed):
+			SceneChanger.change_scene(world_scene)
+			changed = true
