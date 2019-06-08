a2b1aeadfb55392ca67ae0a3ee5f22dcea354460
Renato Böhler <renato.bohler@gmail.com>
2019-06-08 17:10:26 -0300

    Initial death implementation

---
+++ levels/Monster.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var death_scene = "res://menus/TitleScreen.tscn"
+var changed = false
+
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	for body in bodies:		
+		if (body.name == "PlayerBody" and not changed):
+			SceneChanger.change_scene(death_scene)
+			Networking.sync_scene(death_scene)
+			changed = true
